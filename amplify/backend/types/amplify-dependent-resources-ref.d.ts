d1db6d98956329a5499d54df6810e7bb1dc7c4a0
att5197 <att5197@psu.edu>
2023-02-24 19:31:16 -0500

    AlmostThere

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,33 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "FlyLite": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "FlyLite": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "predictions": {
+    "slfModel": {
+      "region": "string",
+      "type": "string"
+    }
+  },
+  "storage": {
+    "s3flylitestorage6936379b": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/ui-components/MainMenu.jsx
+++ src/ui-components/MainMenu.jsx
@@ -7,35 +7,48 @@
 /* eslint-disable */
 import * as React from "react";
 import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { Flex, View } from "@aws-amplify/ui-react";
 import BaseButton from "./BaseButton";
-import { View } from "@aws-amplify/ui-react";
 export default function MainMenu(props) {
   const { b1Label, b2Label, IdentifyEvent, overrides, ...rest } = props;
   return (
     <View
-      width="390px"
+      width="100%"
       height="844px"
-      display="flex"
+      display="block"
       gap="unset"
-      alignItems="center"
-      justifyContent="center"
-      overflow="visible"
+      alignItems="unset"
+      justifyContent="unset"
+      overflow="hidden"
       position="relative"
       padding="0px 0px 0px 0px"
       backgroundColor="lightgrey"
       {...getOverrideProps(overrides, "MainMenu")}
       {...rest}
     >
+      <Flex
+        gap="10px"
+        direction="row"
+        width="unset"
+        height="unset"
+        justifyContent="flex-start"
+        alignItems="flex-start"
+        position="absolute"
+        top="-10px"
+        left="-10px"
+        padding="10px 10px 10px 10px"
+        {...getOverrideProps(overrides, "Frame 2")}
+      ></Flex>
       <BaseButton
         width="143px"
         height="40px"
         display="block"
         gap="unset"
         alignItems="center"
-        justifyContent="unset"
-        position="absolute"
-        top="340px"
-        left="125px"
+        justifyContent="space-evenly"
+        position="relative"
+        top="319px"
+        left="124px"
         padding="0px 0px 0px 0px"
         text={b1Label}
         {...getOverrideProps(overrides, "BaseButton85350")}
@@ -45,11 +58,11 @@ export default function MainMenu(props) {
         height="40px"
         display="block"
         gap="unset"
-        alignItems="unset"
-        justifyContent="unset"
-        position="absolute"
-        top="380px"
-        left="125px"
+        alignItems="center"
+        justifyContent="space-evenly"
+        position="relative"
+        top="382px"
+        left="124px"
         padding="0px 0px 0px 0px"
         text={b2Label}
         {...getOverrideProps(overrides, "BaseButton85375")}
