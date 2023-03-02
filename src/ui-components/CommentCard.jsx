7ec8bca7469b2aff745c8418f7dc09b5f7b5e892
AJBuilder <adam.aj.robertson@gmail.com>
2023-03-02 10:20:40 -0500

    Pulled ui

--- src/ui-components/CommentCard.jsx
+++ src/ui-components/CommentCard.jsx
@@ -14,12 +14,20 @@ export default function CommentCard(props) {
     <Flex
       gap="16px"
       direction="column"
+<<<<<<< HEAD
       width="507px"
+=======
+      width="unset"
+>>>>>>> Pulled ui
       height="unset"
       justifyContent="flex-start"
       alignItems="flex-start"
       position="relative"
+<<<<<<< HEAD
       padding="16px 16px 44px 16px"
+=======
+      padding="16px 16px 16px 16px"
+>>>>>>> Pulled ui
       backgroundColor="rgba(255,255,255,1)"
       {...getOverrideProps(overrides, "CommentCard")}
       {...rest}
@@ -35,6 +43,62 @@ export default function CommentCard(props) {
         alignSelf="stretch"
         position="relative"
         padding="0px 0px 0px 0px"
+<<<<<<< HEAD
+=======
+        {...getOverrideProps(overrides, "Liked")}
+      >
+        <Flex
+          gap="9px"
+          direction="row"
+          width="unset"
+          height="unset"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          shrink="0"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "User Liked")}
+        >
+          <View
+            width="24px"
+            height="24px"
+            {...getOverrideProps(overrides, "MyIcon11196")}
+          ></View>
+          <Text
+            fontFamily="Inter"
+            fontSize="14px"
+            fontWeight="400"
+            color="rgba(92,102,112,1)"
+            lineHeight="24px"
+            textAlign="left"
+            display="block"
+            direction="column"
+            justifyContent="unset"
+            width="unset"
+            height="unset"
+            gap="unset"
+            alignItems="unset"
+            shrink="0"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            whiteSpace="pre-wrap"
+            children="Danny liked this"
+            {...getOverrideProps(overrides, "Danny liked this")}
+          ></Text>
+        </Flex>
+      </Flex>
+      <Flex
+        gap="16px"
+        direction="row"
+        width="unset"
+        height="unset"
+        justifyContent="flex-start"
+        alignItems="flex-start"
+        shrink="0"
+        alignSelf="stretch"
+        position="relative"
+        padding="0px 0px 0px 0px"
+>>>>>>> Pulled ui
         {...getOverrideProps(overrides, "Body")}
       >
         <Flex
@@ -160,7 +224,11 @@ export default function CommentCard(props) {
               <View
                 width="24px"
                 height="24px"
+<<<<<<< HEAD
                 {...getOverrideProps(overrides, "MyIcon")}
+=======
+                {...getOverrideProps(overrides, "MyIcon111107")}
+>>>>>>> Pulled ui
               ></View>
             </Flex>
           </Flex>
@@ -192,6 +260,148 @@ export default function CommentCard(props) {
           ></Text>
         </Flex>
       </Flex>
+<<<<<<< HEAD
+=======
+      <Flex
+        gap="80px"
+        direction="row"
+        width="unset"
+        height="unset"
+        justifyContent="flex-start"
+        alignItems="flex-start"
+        shrink="0"
+        alignSelf="stretch"
+        position="relative"
+        padding="0px 0px 0px 0px"
+        {...getOverrideProps(overrides, "Share111109")}
+      >
+        <Flex
+          gap="16px"
+          direction="row"
+          width="unset"
+          height="unset"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          shrink="0"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "Share111110")}
+        >
+          <View
+            width="24px"
+            height="24px"
+            {...getOverrideProps(overrides, "MyIcon111111")}
+          ></View>
+          <Text
+            fontFamily="Inter"
+            fontSize="16px"
+            fontWeight="400"
+            color="rgba(13,26,38,1)"
+            lineHeight="24px"
+            textAlign="left"
+            display="block"
+            direction="column"
+            justifyContent="unset"
+            letterSpacing="0.01px"
+            width="unset"
+            height="unset"
+            gap="unset"
+            alignItems="unset"
+            shrink="0"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            whiteSpace="pre-wrap"
+            children="99"
+            {...getOverrideProps(overrides, "99111112")}
+          ></Text>
+        </Flex>
+        <Flex
+          gap="16px"
+          direction="row"
+          width="unset"
+          height="unset"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          shrink="0"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "Repost")}
+        >
+          <View
+            width="24px"
+            height="24px"
+            {...getOverrideProps(overrides, "MyIcon111114")}
+          ></View>
+          <Text
+            fontFamily="Inter"
+            fontSize="16px"
+            fontWeight="400"
+            color="rgba(13,26,38,1)"
+            lineHeight="24px"
+            textAlign="left"
+            display="block"
+            direction="column"
+            justifyContent="unset"
+            letterSpacing="0.01px"
+            width="unset"
+            height="unset"
+            gap="unset"
+            alignItems="unset"
+            shrink="0"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            whiteSpace="pre-wrap"
+            children="99"
+            {...getOverrideProps(overrides, "99111115")}
+          ></Text>
+        </Flex>
+        <Flex
+          gap="16px"
+          direction="row"
+          width="unset"
+          height="unset"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          shrink="0"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "Like")}
+        >
+          <View
+            width="24px"
+            height="24px"
+            {...getOverrideProps(overrides, "MyIcon111117")}
+          ></View>
+          <Text
+            fontFamily="Inter"
+            fontSize="16px"
+            fontWeight="400"
+            color="rgba(13,26,38,1)"
+            lineHeight="24px"
+            textAlign="left"
+            display="block"
+            direction="column"
+            justifyContent="unset"
+            letterSpacing="0.01px"
+            width="unset"
+            height="unset"
+            gap="unset"
+            alignItems="unset"
+            shrink="0"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            whiteSpace="pre-wrap"
+            children="99"
+            {...getOverrideProps(overrides, "99111118")}
+          ></Text>
+        </Flex>
+        <View
+          width="24px"
+          height="24px"
+          {...getOverrideProps(overrides, "MyIcon111119")}
+        ></View>
+      </Flex>
+>>>>>>> Pulled ui
     </Flex>
   );
 }
