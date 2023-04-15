75e67c303c4dab20e99e14916bbd002e1614f4ef
Starlord1246 <123395730+att5197@users.noreply.github.com>
2023-04-15 10:49:07 -0400

    Fixed Mobile Sizing

--- src/ui-components/FReference.jsx
+++ src/ui-components/FReference.jsx
@@ -3,8 +3,8 @@ import './FReference.css';
 
 function FReference() {
     return (
-      <div class="min-h-screen bg-white">
-<div class="text-gray-900 pt-12 pr-0 pb-14 pl-0 bg-white items-center justify-center">
+      <div class="min-h-screen bg-white w-full">
+<div class="text-gray-900 pt-12 pr-0 pb-14 pl-0 bg-white items-center justify-center w-full">
 <div class="w-full pt-4 pr-5 pb-6 pl-5 mt-0 mr-auto mb-0 ml-auto space-y-5 sm:py-8 md:py-12 sm:space-y-8 md:space-y-16 mx-auto px-4 sm:px-6 lg:px-8 max-w-full">
 <div class="flex flex-col items-center sm:px-5 md:flex-row">
 <div class="flex flex-col items-start justify-center w-full h-full pt-6 pr-0 pb-6 pl-0 mb-6 md:mb-0 md:w-1/2">
@@ -28,13 +28,13 @@ function FReference() {
       </div>
     </div>
     <div class="grid grid-cols-12 sm:px-5 gap-x-8 gap-y-16">
-    <div class="flex flex-col items-start col-span-12 sm:col-span-6 xl:col-span-4">
+    <div class="container pr-4 flex flex-col items-start col-span-12 sm:col-span-6 xl:col-span-4">
         <img
             src="https://publish.purewow.net/wp-content/uploads/sites/2/2017/08/vineyard-versus-winery-hero.jpg?resize=720%2C404&amp;ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=450&amp;q=60" class="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-"/>
         <p class="bg-green-500 flex items-center leading-none text-sm font-medium text-gray-50 pt-1.5 pr-3 pb-1.5 pl-3
             rounded-full uppercase inline-block">About</p>
         <a class="text-lg font-bold sm:text-xl md:text-2x1">Objectives And Needs</a>
-        <p class="text-m text-black text-left">With the growing threat of infestation from both the Spotted Lanternfly and the Grape Berry
+        <p class="container pr-4 text-m text-black text-left">With the growing threat of infestation from both the Spotted Lanternfly and the Grape Berry
 Moth Dr. Flor has reached out to us in the hopes of creating something that can lift some
 of the burden off her and her colleagues’ shoulders. As of right now Dr. Flor is tasked with
 replying to many growers of grapes within Erie and the surrounding areas concerns in regard to
@@ -58,13 +58,13 @@ to take the time to go back to their office or home to get relevant information.
 there is a need for a mobile system that assists growers with correctly identifying pests and
 communicating with Dr. Flor.</p>
       </div>
-      <div class="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
+      <div class="container pr-4 flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
         <img
             src="https://extension.umd.edu/sites/extension.umd.edu/files/styles/optimized/public/2021-02/HGIC_insects_spottedlanternfly_16x9.jpg?itok=0Ve8TKz8&amp;ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTl8fHxlbnwwfHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=450&amp;q=60" class="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-"/>
         <p class="bg-green-500 flex items-center leading-none text-sm font-medium text-gray-50 pt-1.5 pr-3 pb-1.5 pl-3
             rounded-full uppercase inline-block">Spotted Lanternfly</p>
         <a class="text-lg font-bold sm:text-xl md:text-2xl">What is a SpottedLanternfly?</a>
-        <p class="text-m text-black text-left">A Spotted Lanternfly is an insect known as a planthopper that is 
+        <p class="container pr-4 text-m text-black text-left">A Spotted Lanternfly is an insect known as a planthopper that is 
       originally native to parts of China and Vietnam. It has invasively spread to Japan, South Korea, and the United States of
       America. This insects preferred host plant is known as the tree of heaven, but in the United States it affects agricultural 
       products such as soybeans, grapes, stone fruits, and varieties of apples. This causes it to be a priority concern as it is directly 
@@ -73,11 +73,11 @@ communicating with Dr. Flor.</p>
       </div>
       <div class="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
         <img
-            src="https://entomology.ca.uky.edu/files/styles/panopoly_image_original/public/221a.jpg?itok=fHyHNeUW&amp;ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MzR8fHxlbnwwfHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" class="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-"/>
+            src="https://entomology.ca.uky.edu/files/styles/panopoly_image_original/public/221a.jpg?itok=fHyHNeUW&amp;ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MzR8fHxlbnwwfHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" class="object-cover max-w-xs mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-"/>
         <p class="bg-green-500 flex items-center leading-none text-sm font-medium text-gray-50 pt-1.5 pr-3 pb-1.5 pl-3
             rounded-full uppercase inline-block">Grape Berry Moth</p>
         <a class="text-lg font-bold sm:text-xl md:text-2xl">What is a GrapeBerry Moth?</a>
-        <p class="text-m text-black text-left">The Grape Berry Moth otherwise known as the vine moth is another insect that heavily impacts the agriculture industry as a whole.
+        <p class="container pr-4 text-m text-black text-left">The Grape Berry Moth otherwise known as the vine moth is another insect that heavily impacts the agriculture industry as a whole.
         Primarily it feasts upon grapes in vineyards and is very hard to detect. They are native to Europe, China, India, Japan, Korea, Mongolia, and the Russian far east. 
         However, they have made their way to the United States of America and have begun to become a problem here as well.</p>
       </div>
