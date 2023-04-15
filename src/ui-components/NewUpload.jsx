ed7e241e8476875bb40524ed1b68b192519dbdcf
Starlord1246 <123395730+att5197@users.noreply.github.com>
2023-04-15 08:20:06 -0400

    NewUpload

--- src/ui-components/NewUpload.jsx
+++ src/ui-components/NewUpload.jsx
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FReference.css';
 
 function NewUpload() {
-  return (
-<div className="flex flex-col items-center justify-center w-screen h-screen">
-  <div className="flex flex-col items-center justify-center w-full max-w-lg bg-white rounded-lg shadow-xl">
-    <div className="p-10">
-      <h2 className="text-2xl font-semibold mb-2">Upload your files</h2>
-      <p className="text-xs text-gray-500 mb-4">File should be of format .mp4, .avi, .mov or .mkv</p>
-      <form action="#" className="relative w-full h-48 max-w-xs bg-gray-100 rounded-lg shadow-inner flex items-center justify-center">
-        <input type="file" id="file-upload" className="hidden"/>
-        <label htmlFor="file-upload" className="flex flex-col-reverse items-center justify-center w-full h-full cursor-pointer">
-          <p className="text-xs font-light text-center text-gray-500 mb-2">Drag &amp; Drop your files here</p>
-          <svg className="w-8 h-8 text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"></path>
-          </svg>
-        </label>
-      </form>
-    </div>
-  </div>
-</div>
+  const [imageUrl, setImageUrl] = useState('');
 
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      setImageUrl(reader.result);
+    };
+
+    reader.readAsDataURL(file);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // handle submit logic here, such as sending the image to a server
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen">
+      <div className="relative w-full lg:max-w-sm mb-4">
+        <select className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
+          <option>What Do You Think It Is?</option>
+          <option>A Spotted LanternFly</option>
+          <option>A GrapeBerry Moth</option>
+          <option>I Do Not Know</option>
+        </select>
+      </div>
+
+      <div className="flex flex-col items-center justify-center w-full max-w-lg bg-white rounded-lg shadow-xl">
+        <div className="p-10">
+          <h2 className="text-2xl font-semibold mb-2">Upload your files</h2>
+          <p className="text-xs text-gray-500 mb-4">File should be of format .jpeg, .jpg</p>
+          <form className="relative w-full h-48 max-w-xs bg-gray-100 rounded-lg shadow-inner flex items-center justify-center">
+            <input type="file" id="file-upload" className="hidden" onChange={handleFileUpload} />
+            <label htmlFor="file-upload" className="flex flex-col-reverse items-center justify-center w-full h-full cursor-pointer">
+              {imageUrl ? (
+                <img src={imageUrl} alt="Uploaded file" className="w-full h-full object-contain" />
+              ) : (
+                <>
+                  <p className="text-xs font-light text-center text-gray-500 mb-2">Drag &amp; Drop your files here</p>
+                  <svg className="w-8 h-8 text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"></path>
+                  </svg>
+                </>
+              )}
+            </label>
+          </form>
+          <button type="submit" className="mt-4 bg-green-400 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700">
+              Submit
+            </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
