e3e84e7373946265416a1a3ad3db6e1a09dd660a
Starlord1246 <123395730+att5197@users.noreply.github.com>
2023-04-15 09:51:36 -0400

    InformationFilledIntoTheHomePage

--- src/ui-components/FReference.jsx
+++ src/ui-components/FReference.jsx
@@ -33,9 +33,30 @@ function FReference() {
             src="https://publish.purewow.net/wp-content/uploads/sites/2/2017/08/vineyard-versus-winery-hero.jpg?resize=720%2C404&amp;ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=450&amp;q=60" class="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-"/>
         <p class="bg-green-500 flex items-center leading-none text-sm font-medium text-gray-50 pt-1.5 pr-3 pb-1.5 pl-3
             rounded-full uppercase inline-block">About</p>
-        <a class="text-lg font-bold sm:text-xl md:text-2xl">Objectives And Needs</a>
-        <p class="text-sm text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+        <a class="text-lg font-bold sm:text-xl md:text-2x1">Objectives And Needs</a>
+        <p class="text-m text-black text-left">With the growing threat of infestation from both the Spotted Lanternfly and the Grape Berry
+Moth Dr. Flor has reached out to us in the hopes of creating something that can lift some
+of the burden off her and her colleagues’ shoulders. As of right now Dr. Flor is tasked with
+replying to many growers of grapes within Erie and the surrounding areas concerns in regard to
+possible infestations of their grape clusters and vineyards. There is often difficulty during these
+communications to correctly and efficiently identify these pests whether on a grape cluster or
+in the surrounding areas. This is because if a grower does not know what they are looking
+for to identify the pest they often send in pictures that do not create a certainty. Due to this
+unfortunate series of events, often Dr. Flor and her colleagues have to reach out once again or
+even in some cases go out to the vineyards to help these growers identify these pests and by
+that time it can be too late.
+With these concerns and careful consideration we have narrowed Dr. Flor’s needs into four
+main sub categories that we would like to address. First off there is a need for streamlining the
+correct information to Dr. Flor. Rather than emails that can potentially get lost over time there
+needs to be a centralised system to gather the relevant information. Secondly, there needs to
+be a way that assists in pest identification with as minimal human input into the discussion as possible. This is because the higher the human input the more uncertainty that remains. Thirdly,
+the system should be easily accessible to multiple growers. This is because in order to properly
+track the spread of these pests it needs to be scalable and usable by as many individuals as
+possible. Lastly, whatever we design should be made so that growers can use it while in the
+field. It would defeat the point of making a streamlined identification system if they still had
+to take the time to go back to their office or home to get relevant information. Hence, overall
+there is a need for a mobile system that assists growers with correctly identifying pests and
+communicating with Dr. Flor.</p>
       </div>
       <div class="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
         <img
@@ -43,17 +64,22 @@ function FReference() {
         <p class="bg-green-500 flex items-center leading-none text-sm font-medium text-gray-50 pt-1.5 pr-3 pb-1.5 pl-3
             rounded-full uppercase inline-block">Spotted Lanternfly</p>
         <a class="text-lg font-bold sm:text-xl md:text-2xl">What is a SpottedLanternfly?</a>
-        <p class="text-sm text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+        <p class="text-m text-black text-left">A Spotted Lanternfly is an insect known as a planthopper that is 
+      originally native to parts of China and Vietnam. It has invasively spread to Japan, South Korea, and the United States of
+      America. This insects preferred host plant is known as the tree of heaven, but in the United States it affects agricultural 
+      products such as soybeans, grapes, stone fruits, and varieties of apples. This causes it to be a priority concern as it is directly 
+      affection our food supply chain and hindering farmers livelyhoods. If you find one or a colony of eggs please take action to eradicate the 
+      further spread of this insect.</p>
       </div>
       <div class="flex flex-col items-start col-span-12 space-y-3 sm:col-span-6 xl:col-span-4">
         <img
             src="https://entomology.ca.uky.edu/files/styles/panopoly_image_original/public/221a.jpg?itok=fHyHNeUW&amp;ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MzR8fHxlbnwwfHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" class="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56 btn-"/>
         <p class="bg-green-500 flex items-center leading-none text-sm font-medium text-gray-50 pt-1.5 pr-3 pb-1.5 pl-3
-            rounded-full uppercase inline-block">GrapeBerry Moth</p>
+            rounded-full uppercase inline-block">Grape Berry Moth</p>
         <a class="text-lg font-bold sm:text-xl md:text-2xl">What is a GrapeBerry Moth?</a>
-        <p class="text-sm text-black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+        <p class="text-m text-black text-left">The Grape Berry Moth otherwise known as the vine moth is another insect that heavily impacts the agriculture industry as a whole.
+        Primarily it feasts upon grapes in vineyards and is very hard to detect. They are native to Europe, China, India, Japan, Korea, Mongolia, and the Russian far east. 
+        However, they have made their way to the United States of America and have begun to become a problem here as well.</p>
       </div>
     </div>
     <div>
